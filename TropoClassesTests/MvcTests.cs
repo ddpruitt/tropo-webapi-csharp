f1f842ef9860cf081567636798d2792bae474f75
dfjacobs <dfjacobs@sshores.com>
2012-04-01 19:27:52 -0600

    Change single quotes to double quotes.

--- TropoClassesTests/MvcTests.cs
+++ TropoClassesTests/MvcTests.cs
@@ -17,7 +17,7 @@ namespace TropoClassesTests
         public void Can_Bind_A_Tropo_Session()
         {
             var binder = new SessionModelBinder();
-            var session = @"{'session':{'id':'17bc3787a1623646bcd8440a7d1121d3','accountId':'99999','timestamp':'2011-10-22T21:49:40.794Z','userType':'NONE','initialText':null,'callId':null,'parameters':{'lead_message':'hello world','action':'create'}}}";
+            var session = @"{""session"":{""id"":""17bc3787a1623646bcd8440a7d1121d3"",""accountId"":""99999"",""timestamp"":""2011-10-22T21:49:40.794Z"",""userType"":""NONE"",""initialText"":null,""callId"":null,""parameters"":{""lead_message"":""hello world"",""action"":""create""}}}";
             var request = new Mock<HttpRequestBase>();
             var context = new Mock<HttpContextBase>();
             request.Setup(r => r.InputStream).Returns(new MemoryStream(Encoding.UTF8.GetBytes(session)));
@@ -33,20 +33,20 @@ namespace TropoClassesTests
         public void Can_Bind_A_Tropo_Result()
         {
             var tropoResult =
-@"{'result':{
-     'sessionId':'3FA7C70A1DD211B286B7A583D7B46DDD0xac106207',
-     'state':'ANSWERED',
-     'sessionDuration':1,
-     'sequence':1,
-     'complete':true,
-     'error':null,
-     'actions':{
-        'name':'account_number',
-        'attempts':1,
-        'disposition':'SUCCESS',
-        'confidence':100,
-        'interpretation':'12345',
-        'utterance':'1 2 3 4 5'
+@"{""result"":{
+     ""sessionId"":""3FA7C70A1DD211B286B7A583D7B46DDD0xac106207"",
+     ""state"":""ANSWERED"",
+     ""sessionDuration"":1,
+     ""sequence"":1,
+     ""complete"":true,
+     ""error"":null,
+     ""actions"":{
+        ""name"":""account_number"",
+        ""attempts"":1,
+        ""disposition"":""SUCCESS"",
+        ""confidence"":100,
+        ""interpretation"":""12345"",
+        ""utterance"":""1 2 3 4 5""
      }
   }
 }";
