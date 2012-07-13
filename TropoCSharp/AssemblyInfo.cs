8b8f875ca5d00dcb6a6614527d85e929e83a0414
dfjacobs <dfjacobs@sshores.com>
2012-07-12 20:20:27 -0600

    Update library versions to 1.1.0

--- TropoCSharp/AssemblyInfo.cs
+++ TropoCSharp/AssemblyInfo.cs
@@ -1,21 +1,13 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("TropoCSharp.TropoClasses")]
-[assembly: AssemblyDescription("C# Class Library for the Tropo WabAPI")]
+[assembly: AssemblyDescription("C# Class Library for the Tropo WebAPI")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TropoCSharp")]
-[assembly: AssemblyCopyright("Copyright © 2010")]
+[assembly: AssemblyProduct("Tropo WebAPI")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0 Development")]
+[assembly: AssemblyVersion("1.1")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1 Development")]
